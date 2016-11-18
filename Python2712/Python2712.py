db87077b3a08b4ada7509e5e81a78a53b796fdaf
axeno <axeno@DESKTOP-L4DRDLN>
2016-11-18 19:06:29 +0300

    Add project files.

---
+++ Python2712/Python2712.py
@@ -0,0 +1,17 @@
+from serial import *
+from time import *
+from Tkinter import *
+
+ser = Serial(port='COM3', baudrate=9600, timeout=0)
+
+  
+i = 1000
+
+while(True):
+    ser.write(b'start')
+    sleep(0.05)
+    line = ser.readline()
+    print(line)
+    i -= 1
+    if (i == 0):
+        i = 1000
